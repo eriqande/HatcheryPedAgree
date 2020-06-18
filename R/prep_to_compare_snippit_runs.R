99e110152bc668fb65835b70d788b0bde510f069
Eric C. Anderson <eric.anderson@noaa.gov>
2020-06-18 15:17:21 -0600

    Commit a bunch of stuff.

--- R/prep_to_compare_snippit_runs.R
+++ R/prep_to_compare_snippit_runs.R
@@ -1,67 +1,84 @@
 #' Prepare SNPPIT runs to compare them
 #'
 #' More later
-#'
+#' @param L a list of outputs from different snppit runs.  These should be named by whatever
+#' you want to call each run.
 #' @export
 prep_to_compare_snppit_runs <- function(
-  D,
-  type_levels = c("nSD_full", "nSD_dump10", "wSD_full", "wSD_dump10"),
-  #type_levels = c("nSD_full", "nSD_dump10", "nSD_dumpE", "nSD_Hil", "wSD_full", "wSD_dump10", "wSD_dumpE", "wSD_Hil")
-  GTseqed = sex_and_date$Indiv,
-  GT_hi_miss = gt_miss10p
+  L
 ) {
+
+  type_levels = names(L)
 
   # get the number of the types of analyses
   num_types <- length(type_levels)
+
+  # combine the list into a single tibble
+  D <- bind_rows(L, .id = "type_of_analysis")
 
   D$type_f = factor(D$type_of_analysis, levels = type_levels)
 
   # now, we need to fill in values for the FDR (and p-value) for sorting these
   # things along the x-axis.  Namely we need the values for the "base" case (type_f = 1),
   # if those were NA. I will fill those with the average of the remaining values.
-  # We also need to get the kid's spawn here in there for facetting.
+  # We also need to get the kid's spawn year in there for facetting.
   D2 <- D %>%
-    mutate(kid_spawn_year = year(mdy(kid_date))) %>%
-    arrange(Kid, type_f) %>%
-    group_by(Kid) %>%
+    arrange(kid_hatchery, kid, type_f) %>%
+    group_by(kid_hatchery, kid) %>%
     mutate(
       FDR_comp = ifelse(is.na(FDR), mean(FDR, na.rm = TRUE), FDR),
       Pvalue_comp = ifelse(is.na(Pvalue), mean(Pvalue, na.rm = TRUE), Pvalue)
     ) %>%
-    group_by(Kid) %>%
-    mutate(FDR_comp1 = FDR_comp[1],
+    mutate(FDR_comp1 = FDR_comp[1],  # these are the "composite" scores
            Pvalue_comp1 = Pvalue_comp[1]) %>%
-    arrange(FDR_comp1, Pvalue_comp1, Kid) %>%
+    arrange(FDR_comp1, Pvalue_comp1, kid) %>%
     ungroup() %>%
-    mutate(idx_years_aggregated = as.integer(factor(Kid, levels = unique(Kid))))
+    mutate(idx_all_aggregated = as.integer(factor(kid, levels = unique(kid)))) %>%
+    mutate(
+      kid_min_year = map_int(
+        str_split(kid_year, ","),
+        .f = function(x) min(as.integer(x))
+        ))
 
 
 
-  # Now, give each one an index based on the year that they are in
+  # Now, give each one an index based on the year that they are in, and also
+  # on the year and the hatchery that they are in
   D3 <- D2 %>%
-    group_by(kid_spawn_year) %>%
-    mutate(idx_by_year = as.integer(factor(Kid, levels = unique(Kid)))) %>%
-    ungroup()
+    group_by(kid_min_year) %>%
+    mutate(idx_by_year = as.integer(factor(kid, levels = unique(kid)))) %>%
+    group_by(kid_hatchery, kid_min_year) %>%
+    mutate(idx_by_year_and_hatchery = as.integer(factor(kid, levels = unique(kid)))) %>%
+    ungroup() %>%
+    select(starts_with("idx"), everything())
+
+
+  ### Made it to here so far, and just testing
+  g <- ggplot(D3, aes(x = idx_by_year_and_hatchery, y = FDR + 0.05 * (as.integer(type_f) - 1), colour = type_f)) +
+    geom_hline(yintercept = 0.05, colour = "white") +
+    geom_point(shape = 21, stroke = 0.2, size = 1.2) +
+    facet_grid(kid_min_year ~ kid_hatchery) +
+    scale_color_manual(values = c("blue", "red"))
+  ggsave(g, filename = "development/outputs/sad-no-sad-fdr-hathchery-by-year-facet-grid.pdf",
+         width = 25, height = 14)
 
   # now, we want to pick out those indivs that are not assigned to the same
   # parents as the base case (type_f == 1) when both of them are non-missing
   base_casers <- D3 %>%
-    select(type_f, Kid, Pa, Ma) %>%
-    rename(basePa = Pa,
-           baseMa = Ma) %>%
+    select(type_f, kid, pa, ma) %>%
+    rename(base_pa = pa,
+           base_ma = ma) %>%
     filter(as.integer(type_f) == 1) %>%
     select(-type_f)
 
   D3_plus <- D3 %>%
-    left_join(base_casers, by = "Kid")
+    left_join(base_casers, by = "kid")
 
   # finally, we want to get some things to make colors
   D4 <- D3_plus %>%
     mutate(
       parent_compatibility = case_when(
-        !is.na(Ma) & !is.na(Pa) & !is.na(basePa) & !is.na(baseMa) & ((Ma != basePa & Ma != baseMa) | (Pa != basePa & Pa != baseMa)) ~ "MaPa_not_matching_base_case",
-        (Ma %in% GT_hi_miss) | (Pa %in% GT_hi_miss) ~ "Ma_or_Pa_is_potential_GT_Seq_hi_misser",
-        !(Kid %in% GTseqed & Ma %in% GTseqed & Pa %in% GTseqed) ~ "full_trio_not_GTseq-ed",
+        (!is.na(ma) & !is.na(pa) & !is.na(base_pa) & !is.na(base_ma)) & !((ma == base_ma & pa == base_pa) | (pa == base_ma & ma == base_pa)) ~ "ma_pa_not_matching_base_case",
         is.na(ma_date) | is.na(pa_date)       ~ "date_not_recorded",
         is.na(ma_sex)  | is.na(pa_sex)        ~ "sex_not_recorded",
         pa_sex == ma_sex & pa_date != ma_date ~ "sex_and_date_mismatch",
