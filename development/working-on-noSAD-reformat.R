c920f084c572ec2227bb8d371c0bcaa6e7494170
Eric C. Anderson <eric.anderson@noaa.gov>
2020-08-26 10:25:08 -0600

    random

--- development/working-on-noSAD-reformat.R
+++ development/working-on-noSAD-reformat.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(HatcheryPedAgree)
 
 
-S <- read_rds("private/rrsh_metadata.rds")
+S <- readr::read_rds("private/rrsh_metadata.rds")
 # get a data set to play with:
 D <- slurp_snppit(
   DIR = "~/Documents/UnsyncedData/HatcheryPedAgreeOutputs/RussianRiver-July-6-2020/snppit-run-noSAD",
@@ -11,3 +11,4 @@ D <- slurp_snppit(
   )
 
 
+D2 <- reformat_no_sex_or_date_results(D)
